1fe53ffadf82bc8399a4b9f487f75514d8997006
Jcs254 <144740202+Jcs254@users.noreply.github.com>
2023-12-21 23:51:35 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-// Your changes
-=======
-// Remote changes
->>>>>>> COMMIT_HASH
-
 resource "aws_instance" "server1" {
   instance_type = var.instance_type
   ami = data.aws_ami.ami1.id
